299fe4fa5c6bc987392b83ea110642526193b1ee
functionzz <jamie.hu.132@gmail.com>
2026-08-06 19:56:49 -0400

    tmp trial yml

--- .github/scripts/fetch_commit_tag.py
+++ .github/scripts/fetch_commit_tag.py
@@ -10,8 +10,8 @@ def main():
         response = requests.get(target_url, timeout=10)
         response.raise_for_status()
 
-        text = response.text
-        print(f"Successfully fetched data for: {text}")
+        commit = response.text
+        print(f"Successfully fetched data for: {commit}")
 
     except requests.exceptions.RequestException as e:
         print(f"Error fetching URL: {e}")
@@ -19,8 +19,15 @@ def main():
             print("release=false", file=fh)
         return
 
+# fetch commit sha
 
-    if text.startswith("v20"):
+# get commit tag, check if it includes v20*
+
+# if yes, end
+# if no, tag commit with generated semver
+# release with generated semver as the title.
+
+    if commit.startswith("v20"):
         with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
             print("release=false", file=fh)
         return
@@ -30,6 +37,7 @@ def main():
 
 
     with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        print(commit)
         print(f"version={version}", file=fh)
         print("release=true", file=fh)
 
